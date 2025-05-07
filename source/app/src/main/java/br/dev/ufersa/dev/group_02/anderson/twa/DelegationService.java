fdf313cbbf5e02e836f692d2b33e50f6e83dbb12
anddfreitas <andersonabdfp@gmail.com>
2025-05-07 15:39:49 -0300

    att 2

---
+++ source/app/src/main/java/br/dev/ufersa/dev/group_02/anderson/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package br.dev.ufersa.dev.group_02.anderson.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
